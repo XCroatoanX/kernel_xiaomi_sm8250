775d0a65e0ed0b9e552bf81d5a14c84794ce70bb
XCroatoanX <stasyukartem2@gmail.com>
2023-11-29 00:02:58 +0100

    revert

--- drivers/esoc/esoc-mdm-drv.c
+++ drivers/esoc/esoc-mdm-drv.c
@@ -71,8 +71,6 @@ int esoc_set_boot_fail_action(struct esoc_clink *esoc_clink, u32 action)
 				action);
 		return -EINVAL;
 	}
-	if (action == BOOT_FAIL_ACTION_PANIC)
-		action = BOOT_FAIL_ACTION_DEF;
 
 	if (!mdm_drv) {
 		esoc_mdm_log("esoc-mdm driver not present\n");
@@ -393,7 +391,7 @@ static int mdm_handle_boot_fail(struct esoc_clink *esoc_clink, u8 *pon_trial)
 	if (*pon_trial == atomic_read(&mdm_drv->n_pon_tries)) {
 		esoc_mdm_log("Reached max. number of boot trials\n");
 		atomic_set(&mdm_drv->boot_fail_action,
-					BOOT_FAIL_ACTION_DEF);
+					BOOT_FAIL_ACTION_NOP);
 	}
 
 	switch (atomic_read(&mdm_drv->boot_fail_action)) {
